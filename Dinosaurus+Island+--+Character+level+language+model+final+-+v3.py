19493002242c2730ebd935db9c0d7c6e1c21dc86
hritik9456 <57741385+hritik9456@users.noreply.github.com>
2019-12-03 10:06:46 +0530

    Update Dinosaurus+Island+--+Character+level+language+model+final+-+v3.py

--- Dinosaurus+Island+--+Character+level+language+model+final+-+v3.py
+++ Dinosaurus+Island+--+Character+level+language+model+final+-+v3.py
@@ -611,64 +611,7 @@ parameters = model(data, ix_to_char, char_to_ix)
 # 
 # <img src="images/mangosaurus.jpeg" style="width:250;height:300px;">
 
-# ## 4 - Writing like Shakespeare
-# 
-# The rest of this notebook is optional and is not graded, but we hope you'll do it anyway since it's quite fun and informative. 
-# 
-# A similar (but more complicated) task is to generate Shakespeare poems. Instead of learning from a dataset of Dinosaur names you can use a collection of Shakespearian poems. Using LSTM cells, you can learn longer term dependencies that span many characters in the text--e.g., where a character appearing somewhere a sequence can influence what should be a different character much much later in ths sequence. These long term dependencies were less important with dinosaur names, since the names were quite short. 
-# 
-# 
-# <img src="images/shakespeare.jpg" style="width:500;height:400px;">
-# <caption><center> Let's become poets! </center></caption>
-# 
-# We have implemented a Shakespeare poem generator with Keras. Run the following cell to load the required packages and models. This may take a few minutes. 
 
-# In[12]:
-
-from __future__ import print_function
-from keras.callbacks import LambdaCallback
-from keras.models import Model, load_model, Sequential
-from keras.layers import Dense, Activation, Dropout, Input, Masking
-from keras.layers import LSTM
-from keras.utils.data_utils import get_file
-from keras.preprocessing.sequence import pad_sequences
-from shakespeare_utils import *
-import sys
-import io
-
-
-# To save you some time, we have already trained a model for ~1000 epochs on a collection of Shakespearian poems called [*"The Sonnets"*](shakespeare.txt). 
-
-# Let's train the model for one more epoch. When it finishes training for an epoch---this will also take a few minutes---you can run `generate_output`, which will prompt asking you for an input (`<`40 characters). The poem will start with your sentence, and our RNN-Shakespeare will complete the rest of the poem for you! For example, try "Forsooth this maketh no sense " (don't enter the quotation marks). Depending on whether you include the space at the end, your results might also differ--try it both ways, and try other inputs as well. 
-# 
-
-# In[13]:
-
-print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
-
-model.fit(x, y, batch_size=128, epochs=1, callbacks=[print_callback])
-
-
-# In[ ]:
-
-# Run this cell to try with different inputs without having to re-train the model 
-generate_output()
-
-
-# The RNN-Shakespeare model is very similar to the one you have built for dinosaur names. The only major differences are:
-# - LSTMs instead of the basic RNN to capture longer-range dependencies
-# - The model is a deeper, stacked LSTM model (2 layer)
-# - Using Keras instead of python to simplify the code 
-# 
-# If you want to learn more, you can also check out the Keras Team's text generation implementation on GitHub: https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py.
-# 
-# Congratulations on finishing this notebook! 
-
-# **References**:
-# - This exercise took inspiration from Andrej Karpathy's implementation: https://gist.github.com/karpathy/d4dee566867f8291f086. To learn more about text generation, also check out Karpathy's [blog post](http://karpathy.github.io/2015/05/21/rnn-effectiveness/).
-# - For the Shakespearian poem generator, our implementation was based on the implementation of an LSTM text generator by the Keras team: https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py 
-
-# In[ ]:
 
 
 
